6a9bd6dd8798c33b088f0f08699b7674f05d6ae0
Travis Sudderth <travissudderth@gmail.com>
2019-11-29 15:04:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
 function dwarfRollCall(dwarves) {
-}
+  let listDwarves = []
+  for (let i = 0; i<dwarves.length; i++){
+    listDwarves.push('${i+1}. ${dwarves[i]}')
+  }
+  return listDwarves.join("");
+
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(words){
 }
 
 function findTheCheese (foods) {
